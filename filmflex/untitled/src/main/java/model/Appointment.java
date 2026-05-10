d49f9f1f3695a35f4b9fd83008395add61d3dc98
P T J Thakshila <it25100393@my.sliit.lk>
2026-05-10 16:10:38 +0530

    gukcmokw

---
+++ filmflex/untitled/src/main/java/model/Appointment.java
@@ -0,0 +1,72 @@
+package com.example.beautysalonbookingsystem.model;
+
+
+public class Appointment {
+
+    private String appointmentId;
+    private String customerName;
+    private String phone;
+    private String serviceName;
+    private String date;
+    private String time;
+    private String status;
+
+    public Appointment() {
+    }
+
+    public String getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(String appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
