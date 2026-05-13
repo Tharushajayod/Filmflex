9fb096981072f61a4f7d7ebc63205a26e085f6bf
IT25102224 <it25102224@my.sliit.lk>
2026-05-14 01:00:32 +0530

    updaete payement gateway date bug

---
+++ filmflex/untitled/src/main/java/filter/AdminAuthFilter.java
@@ -0,0 +1,35 @@
+package filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/adminDashboard.html", "/movieManagement.html", "/api/movies/*", "/api/movies"})
+public class AdminAuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String method = req.getMethod();
+        boolean movieReadOnly = req.getServletPath().startsWith("/api/movies") && "GET".equalsIgnoreCase(method);
+        if (movieReadOnly) {
+            chain.doFilter(request, response);
+            return;
+        }
+        HttpSession session = req.getSession(false);
+        boolean isAdmin = session != null && session.getAttribute("admin") != null;
+        if (isAdmin) {
+            chain.doFilter(request, response);
+        } else if (req.getServletPath().endsWith(".html")) {
+            res.sendRedirect("adminLogin.html");
+        } else {
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Admin login required");
+        }
+    }
+
+    @Override public void init(FilterConfig filterConfig) { }
+    @Override public void destroy() { }
+}
